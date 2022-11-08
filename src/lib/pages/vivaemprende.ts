54ff7d4b9d21d09bfadc0626f1d8468fe58b6a77
Jesus Bauza <jesusbauza81@gmail.com>
2022-11-08 18:54:05 -0400

    Update vivaemprende.ts

--- src/lib/pages/vivaemprende.ts
+++ src/lib/pages/vivaemprende.ts
@@ -12,7 +12,7 @@ export default {
       type: 'hero',
       node: {
         logo: '/pages/vivaemprende/logo-new.png',
-        image: 'hidden',
+        image: '',
         callAction: 'Quiero postularme',
         callActionTextColor: 'white',
         backgroundColor: 'rgb(0, 142, 146)',
